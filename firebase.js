6ce23ca928d4c7e706c1708dd67ddce29fed2566
James Livolsi <james.livolsi08@gmail.com>
2019-04-23 19:58:24 -0400

    bump

--- firebase.js
+++ firebase.js
@@ -3,7 +3,7 @@
 const admin = require('firebase-admin')
 const config = require('./config.js')
 
-const serviceAccount = JSON.parse(process.env.FIREBASE_PRODUCTION_SERVICE_ACCT)
+const serviceAccount = JSON.parse(process.env.FIREBASE_PRODUCTION_SERVICE_ACCT) //
 
 const databaseURL = config.firebase.databaseURL
 
