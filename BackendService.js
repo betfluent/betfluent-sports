e6118ea9bf21dad8b8326436ca973eb61d041d4c
James Livolsi <james.livolsi08@gmail.com>
2019-05-19 20:55:47 -0400

    Use idToken to auth against backend

--- BackendService.js
+++ BackendService.js
@@ -6,15 +6,19 @@ const request = require('request').defaults({
 })
 
 const BASE_URL = 'https://boston-02108.herokuapp.com/api/'
+const GOOGLE_API = `https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyCustomToken?key=${process.env.WEB_API_KEY}`
 
-const closeBet = (betId) => {
+const closeBet = async (betId) => {
   const session = {
     id: firebase.database().ref().push().key,
     serviceType: 'BET_RESULT',
     request: betId
   }
+
+  const token = await getIdToken()
+
   request.post({
-    headers: { token: process.env.ADMIN_KEY },
+    headers: { token },
     url: BASE_URL + 'v1/manager/result',
     body: session
   }, function (error, response, body) {
@@ -23,6 +27,24 @@ const closeBet = (betId) => {
       return
     }
     console.log('----- CLOSE BET:', body)
+  })
+}
+
+const getIdToken = () => {
+  return new Promise(res => {
+    request.post({
+      url: GOOGLE_API,
+      body: {
+        token: process.env.ADMIN_KEY,
+        returnSecureToken: true
+      }
+    }, function (error, response, body) {
+      if (error) {
+        console.log('---------- ERROR:', error)
+        return
+      }
+      res(body.idToken)
+    })
   })
 }
 
