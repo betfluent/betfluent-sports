829ee957c536a2ed19458606d9a8dd777d6ba420
James Livolsi <james.livolsi08@gmail.com>
2019-05-19 21:37:15 -0400

    Use permanent ID token

--- BackendService.js
+++ BackendService.js
@@ -6,7 +6,6 @@ const request = require('request').defaults({
 })
 
 const BASE_URL = 'https://boston-02108.herokuapp.com/api/'
-const GOOGLE_API = `https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyCustomToken?key=${process.env.WEB_API_KEY}`
 
 const closeBet = async (betId) => {
   const session = {
@@ -15,10 +14,8 @@ const closeBet = async (betId) => {
     request: betId
   }
 
-  const token = await getIdToken()
-
   request.post({
-    headers: { token },
+    headers: { token: process.env.ADMIN_KEY },
     url: BASE_URL + 'v1/manager/result',
     body: session
   }, function (error, response, body) {
@@ -27,24 +24,6 @@ const closeBet = async (betId) => {
       return
     }
     console.log('----- CLOSE BET:', body)
-  })
-}
-
-const getIdToken = () => {
-  return new Promise(res => {
-    request.post({
-      url: GOOGLE_API,
-      body: {
-        token: process.env.ADMIN_KEY,
-        returnSecureToken: true
-      }
-    }, function (error, response, body) {
-      if (error) {
-        console.log('---------- ERROR:', error)
-        return
-      }
-      res(body.idToken)
-    })
   })
 }
 
