d4d31c11385fbf05e843f0ac4407162b9a2065ef
James Livolsi <james.livolsi08@gmail.com>
2019-06-08 10:42:14 -0400

    close bets

--- index.js
+++ index.js
@@ -27,5 +27,12 @@ donbest.start()
 admin.auth().createCustomToken(process.env.ADMIN_KEY)
     .then(customToken => {
         firebase.auth().signInWithCustomToken(customToken)
+            .then(async () => {
+                const snap = await firebase.database().ref('mlb/games').orderByChild('status').equalTo('closed').once('value')
+                const games = Object.values(snap.val())
+                games.forEach(game => {
+                betManager.closeBetsForGame(game)  
+                })
+            })
             .catch(error => console.log(error))
     })
